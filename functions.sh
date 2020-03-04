886269659c27c46eedb2f812ad1e0bf406b77de0
chenchuk77 <chenchuk@gmail.com>
2020-03-04 09:40:22 +0200

    adding bash functions

--- functions.sh
+++ functions.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#
+# functions to be used in dev/testing, source this file in your ~/.bashrc
+# or source it to the current shell by:
+#
+# $ source functions.sh
+# 
+
+
 function dps {
   docker ps --format 'table {{.Names}}\t{{.Status}}\t{{.Ports}}'
 }
